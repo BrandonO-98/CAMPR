f1d6dc1acf067af8b0874de2037d55c76fa3ec80
BrandonO-98 <brandonshew98@gmail.com>
2022-07-14 12:19:46 -0400

    Fixed Some security Issues using helmet and sanitization packages.

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
 
 const userRoutes = require('./routes/userRoutes')
 const campgroundRoutes = require('./routes/campgroundRoutes')
@@ -40,13 +42,18 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(mongoSanitize())
 
 const sessionConfig = {
+  name: 'session',
   secret: 'thisismysecret', 
   resave: false,
   saveUninitialized: true,
   cookie: {
+    // cookies cannot be accessed via JS
     httpOnly: true,
+    // cookies can only be accessed over https
+    // secure: true,
     // give the cookie an expiry, if this is not defined
     //a logged in user could be logged in forever
     expires: Date.now() + 1000 * 60 * 60 *24 *7,
@@ -55,6 +62,60 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash())
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net/",
+  "https://res.cloudinary.com/da6su05rx/"
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/",
+  "https://res.cloudinary.com/da6su05rx/"
+];
+const connectSrcUrls = [
+  "https://*.tiles.mapbox.com",
+  "https://api.mapbox.com",
+  "https://events.mapbox.com",
+  "https://res.cloudinary.com/da6su05rx/"
+];
+const fontSrcUrls = [ "https://res.cloudinary.com/da6su05rx/" ];
+
+app.use(
+  helmet({
+      contentSecurityPolicy: {
+          directives : {
+              defaultSrc : [],
+              connectSrc : [ "'self'", ...connectSrcUrls ],
+              scriptSrc  : [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+              styleSrc   : [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+              workerSrc  : [ "'self'", "blob:" ],
+              objectSrc  : [],
+              imgSrc     : [
+                  "'self'",
+                  "blob:",
+                  "data:",
+                  "https://res.cloudinary.com/da6su05rx/", 
+                  "https://images.unsplash.com/"
+              ],
+              fontSrc    : [ "'self'", ...fontSrcUrls ],
+              // mediaSrc   : [ "https://res.cloudinary.com/da6su05rx/" ],
+              // childSrc   : [ "blob:" ]
+          }
+      },
+      crossOriginEmbedderPolicy: false
+  })
+);
+
 app.use(passport.initialize())
 app.use(passport.session())
 // use Local authentication
